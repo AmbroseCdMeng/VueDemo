9c5158f0e270acf37c3714f3b5457e4c8b3b0960
MengChengdu <mcd.951026@foxmail.com>
2019-05-27 18:58:14 +0800

    update vuedemo

--- vue_hello/src/components/User.vue
+++ vue_hello/src/components/User.vue
@@ -1,11 +1,23 @@
 <template>
-    
+  <!--Router 中路由设置 user/:id 。: 为路径的定义参数。匹配到路由时，参数值会被设置到 this.$route.params 中，可以在组件中使用-->
+  <div>
+    <span>用户页面。Hello {{ $route.params.id}}</span>
+
+    <p>路由跳转</p>
+    <router-link :to="{name:'user', params:{id:'Meng'}}">Jump to router named User</router-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "User"
-    }
+  import router from 'vue-router';
+
+  export default {
+    name: "User"
+  }
+
+  //router.go(1);
+  //router.push('home');
+  // router.push({path:'home'});
 </script>
 
 <style scoped>
--- vue_hello/src/router/index.js
+++ vue_hello/src/router/index.js
@@ -1,19 +1,36 @@
-
 //引入相关代码包
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import User from "@/components/User";
+import VIP from "@/components/VIP";
 
 //使用引入的包
 Vue.use(Router);
 
 //定义路由
 export default new Router({
+  //mode: 'history',
+  mode: 'hash',//Vue 默认的模式是 hash。其特点是 URL 后会有一个 #，目的是在访问地址中传参。方便对浏览器前进、后退、跳转功能使用
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+
+    {
+      path: '/user/:id',
+      component: User,
+      /*命名路由，方便标识*/
+      name: 'user',
+      /*嵌套路由*/
+      children: [
+        {
+          path: 'vip',
+          component: VIP,
+        }
+      ]
     }
   ]
 })
